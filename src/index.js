21c5ad72441902d6348664064862452a34e1fae2
martin <martin_v_q@hotmail.com>
2022-07-01 17:54:15 -0500

    remove the browserRouter component

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { MainApp } from './routes/MainApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <BrowserRouter>
+  <React.StrictMode>
+    {/* <BrowserRouter> */}
       <HashRouter>
         <MainApp />
       </HashRouter>
-    </BrowserRouter>
-  // </React.StrictMode>
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
